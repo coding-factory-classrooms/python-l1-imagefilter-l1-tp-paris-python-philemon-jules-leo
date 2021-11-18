b4e8ad0da3187ad0137c0d9af464ef3234f15988
Philémon Wild <philemon.wild@gmail.com>
2021-11-18 12:42:33 +0100

    zeteam color input

--- filtres/ZeTeam.py
+++ filtres/ZeTeam.py
@@ -1,7 +1,8 @@
 import cv2
 
-def zeTeam_filter(img):
+def zeTeam_filter(img,color):
     title_text = "Philemon Jules Leo"
     title_font = cv2.FONT_HERSHEY_SIMPLEX
-    zTf = cv2.putText(img,title_text,(60,350),title_font,1,(255, 140, 234),3)
+    title_color = color
+    zTf = cv2.putText(img,title_text,(60,350),title_font,1,title_color,3)
     return zTf
