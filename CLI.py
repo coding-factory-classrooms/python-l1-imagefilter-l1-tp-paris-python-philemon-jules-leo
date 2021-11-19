50826361b23e9cad32a703c348bd482c19a8180e
Jules <jules.duarte@edu.esiee-it.fr>
2021-11-19 09:44:35 +0100

    argparse qui marche

--- CLI.py
+++ CLI.py
@@ -3,7 +3,6 @@ import core
 import logger as l
 import configparser
 import argparse
-import sys
 
 config = configparser.ConfigParser()
 
@@ -13,83 +12,84 @@ config.read('config.ini')
 conf = {"input": '',
         'output': '',
         'filters': '',
-        'log_file': config['DEFAULT']['log_file']
+        'log_file': config['DEFAULT']['log_file'],
+        'type':'',
+        'output_video':'',
         }
 
 l.init_log(conf['log_file'])
-args = sys.argv
-# def check_args():
-#     parser = argparse.ArgumentParser()
-#     parser.add_argument('-origine', type=str, help='choose the origin file', default="")
-#     parser.add_argument('-i', '--input', help='initiate input argument')
-#     parser.add_argument('-output_path', type=str, help='choose the output file', default="")
-#     parser.add_argument('-o', '--output', help='initiate output argument')
-#     parser.add_argument('-filter_choice', type=str, help='choose the filters you want to use', default="")
-#     parser.add_argument('-f', '--filter', help='initiate filters argument')
-#     parser.add_argument('-config_file_choice', type=str, help='choose the config_file you want to use', default="")
-#     parser.add_argument('-c', '--config_file', help='initiate config_file argument')
-#     parser.add_argument('-l', '--log', help='show the whole log file')
-#
-#     args = parser.parse_args()
-#
-#     if args.input:
-#         print(args.input)
-#         print(args.origine)
-#         l.log('the argument for the input file has been taken')
-#         conf["input"] = args.input
-#
-#     elif args.output:
-#         print(args.output)
-#         l.log('the argument for the output file has been taken')
-#         conf["output"] = args.output_path
-#
-#     elif args.filter:
-#         l.log('the argument for the filters has been taken')
-#         conf["filters"] = args.filter_choice
-#
-#     elif args.config_file:
-#         l.log('the argument for the input file has been taken')
-#         config.read(f'{args.config_file_choice}')
-#         conf['input'] = config['DEFAULT']['input']
-#         conf['output'] = config['DEFAULT']['output']
-#         conf['filters'] = config['DEFAULT']['filters']
-#
-#     elif args.log:
-#         l.dump_log()
-#
-#     else:
-#         print('aucun argument spécifié')
-#
-# if __name__ == '__main__':
-#     check_args()
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('-i', '--input', type=str, help='initiate input argument')
+parser.add_argument('-o', '--output', type=str,  help='initiate output argument')
+parser.add_argument('-f', '--filters', type=str, help='initiate filters argument')
+parser.add_argument('-c', '--config_file', type=str,  help='initiate config_file argument')
+parser.add_argument('-l', '--log', type=str,  help='show the whole log file')
+parser.add_argument('-d', '--display_filters', type=str,  help='show the filters available')
+parser.add_argument('-v', '--video', type=str,  help='show the filters available')
+parser.add_argument('-O', '--output_video', type=str,  help='show the filters available')
 
 
+args = parser.parse_args()
 
-for i, arg in enumerate(args):
-    if arg == '-i':
-        l.log('the argument for the input file has been taken')
-        conf["input"] = args[i + 1]
-    if arg == '-o':
-        l.log('the argument for the outpout file has been taken')
-        conf['output'] = args[i + 1]
-    if arg == '-filters':
-        l.log('the argument for the filters has been taken')
-        conf['filters'] = args[i + 1]
-    if arg == '-log':
-        l.dump_log()
-    if arg == '--config-file':
-        l.log('the arguments used are those of the config file')
-        config.read(f'{args[i+1]}')
-        conf['input'] = config['DEFAULT']['input']
-        conf['output'] = config['DEFAULT']['output']
-        conf['filters'] = config['DEFAULT']['filters']
-    if arg == '-display_filters':
-        l.log('diplaying filters')
-        print("available filters: grayscale | zeTeam | dilate:* | blur:* , replace * with a number to adjust filter intensity")
+if args.config_file:
+
+    l.log('the argument for the config file has been taken')
+    config.read(f'{args.config_file}')
+    conf['input'] = config['DEFAULT']['input']
+    conf['output'] = config['DEFAULT']['output']
+    conf['filters'] = config['DEFAULT']['filters']
+
+if args.input:
+
+    l.log('the argument for the input file has been taken')
+    conf["input"] = args.input
+
+if args.output:
+
+    l.log('the argument for the output file has been taken')
+    conf["output"] = args.output
+
+if args.filters:
+    l.log('the argument for the filters has been taken')
+    conf["filters"] = args.filters
+
+if args.log:
+    l.dump_log()
+
+if args.display_filters:
+    l.log('diplaying filters')
+    print("available filters: grayscale | zeTeam | dilate:* | blur:* , replace * with a number to adjust filter intensity")
+
+if args.output_video:
+    l.log('the argument for the output_video file has been taken')
+    conf['output_video']= args.output_video
+
+if args.video:
+    if conf['output_video'] != '':
+        core.get_video(args.video, conf['output_video'])
+        if args.filters:
+            l.log('the argument for the filters has been taken')
+            config.read(f'config.ini')
+            conf['filters'] = args.filters
+            args_filter = conf['filters']
+            arg_filter = args_filter.split('|')
+            conf['input'] = core.get_image_video(conf['output_video'])
+            image = conf['input']
+            core.apply_filters(image, conf['output'], arg_filter)
+        else:
+            print('the filters argument is missing')
+    else:
+        print('the argument for the output_video file is missing')
+
+else:
+    args_filter = conf['filters']
+    arg_filter = args_filter.split('|')
+    images = core.get_images(conf["input"])
+    core.apply_filters(images, conf['output'], arg_filter)
+
+
 
 # inout existe ?
 
-args_filter = conf['filters']
-arg_filter = args_filter.split('|')
-images = core.get_images(conf["input"])
-core.apply_filters(images,conf['output'], arg_filter)
--- core.py
+++ core.py
@@ -1,4 +1,5 @@
 import cv2
+from PIL import Image
 
 import logger as l
 from filtres import gray
@@ -7,6 +8,9 @@ from filtres import blur
 from filtres import ZeTeam
 from pathlib import Path
 import os
+import glob
+
+
 
 
 def apply_filters(images, odir, filters):
@@ -20,6 +24,7 @@ def apply_filters(images, odir, filters):
     for img_path in images:
         image = cv2.imread(img_path)
         name = os.path.basename(img_path)
+        l.log(f'Opening image = {img_path}')
         for f in filters:
             split = f.split(':')
             filter_name = split[0]
@@ -27,7 +32,7 @@ def apply_filters(images, odir, filters):
                 l.log(f'the grayscale filter has been successfully applied on the picture {name}')
                 image = gray.gray_filter(image)
             elif filter_name == 'blur':
-                if len(split) > 1:
+                if len(split) > 1 and split[1] != '' and int(split[1])%2 != 0:
                     pow = int(split[1])
                     l.log(f'the blur filter has been successfully applied on the picture {name} ')
                     image = blur.blur_filter(image, pow)
@@ -41,9 +46,10 @@ def apply_filters(images, odir, filters):
                 else:
                     print('il manque un argument')
             elif filter_name == 'zeTeam':
-                l.log(f'successfully applied zeTeam filter on {name}')
-                image = ZeTeam.zeTeam_filter(image)
-
+                if len(split) > 1:
+                    color = int(split[1], 16)
+                    l.log(f'successfully applied zeTeam filter on {name}')
+                    image = ZeTeam.zeTeam_filter(image, color)
 
         # gri = gray.gray_filter(image)
         # dil = dilate.dilate_filter(gri,5)
@@ -57,20 +63,88 @@ def apply_filters(images, odir, filters):
             os.makedirs(odir)
         output_file = f"{odir}/{name}"
         cv2.imwrite(output_file, image)
+        l.log(f'saving result image in {output_file}')
+
+    # name = nom du dossier output
+    # odir = chemin d'accès
+
 
 def get_images(fdir):
     """
     parcours le dossier et récupère les images
-    :param dir: dossier à parcourir
+    :param fdir: dossier à parcourir
     :return: une suite d'images du dossier
     """
     images = []
     pathlist = Path(fdir).glob('**/*.[jpg][png]*')
+    for path in pathlist:
+        image = str(path)
+        images.append(image)
+    return images
+
+def get_video(dir, odir):
+
+    cap = cv2.VideoCapture(dir)
+
+    if not os.path.exists(odir):
+        os.makedirs(odir)
+
+    current_frame = 0
+
+    FPS = cap.get(cv2.CAP_PROP_FPS)
+    cap.set(cv2.CAP_PROP_FPS, FPS)
+
+    bool = True
+    while(bool):
+
+        ret, frame = cap.read()
+
+        name = f'./{odir}/frame' + str(current_frame) + '.jpg'
+        print('Creating...' + name)
+        bool = ret
+        try:
+            cv2.imwrite(name, frame)
+
+        except:
+            continue
+
+        current_frame += 1
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+def get_image_video(dir):
+
+    images = []
+    pathlist = Path(dir).glob('**/*.[jpg][png]*')
     l.log('the images has been successfully taken')
     for path in pathlist:
         image = str(path)
         images.append(image)
     return images
+
+def make_gif(odir):
+    frames = [Image.open(image) for image in glob.glob(f"{odir}/*.jpg")]
+    frame_one = frames[0]
+    frame_one.save("filtered_images.gif", format="GIF", append_images=frames,
+                   save_all=True, duration=1000, loop=0)
+
+    # frames = []
+    # odir = os.listdir(odir)
+    # for gif in odir:
+    #     if not gif.endswith('.jpg'):
+    #         odir.remove(gif)
+    #
+    # # pathlist2 = Path(odir).glob('**/*.[jpg][png]*')
+    # # print(pathlist2)
+    # for gif in odir:
+    #     # frame = str(gif)
+    #     frames.append(gif)
+    #     print(frames)
+    # frame_one = frames[0]
+    # frame_one = frame_one.save("my_awesome.gif", format="GIF", append_images=frames,
+    #             save_all=True, duration=100, loop=0)
 
 # images = get_images('Images')
 # print(images)
